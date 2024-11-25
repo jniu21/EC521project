f14df9d09c1180f36a9482fe5fa6d0e7a044354b
JiaweiXiang <x1angjw@bu.edu>
2024-11-24 21:58:47 -0500

    extension

---
+++ extension/popup/popup.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const toggle = document.getElementById('toggleSwitch');
+    const status = document.getElementById('status');
+
+    const { isEnabled } = await chrome.storage.local.get('isEnabled');
+    toggle.checked = isEnabled;
+    status.textContent = isEnabled ? 'Protection is On' : 'Protection is Off';
+
+    toggle.addEventListener('change', async () => {
+        const isEnabled = toggle.checked;
+        await chrom.storage.local.set({ isEnabeld });
+        status.textContent = isEnabled ? 'Protection is On' : 'Protection is Off';
+
+        chrome.runtime.sendMessage({type: 'TOGGLE_PROTECTION', isEnabled});
+    });
+});
