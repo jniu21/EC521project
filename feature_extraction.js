39240ac2ee1e979e3ed25ecaed5196ca096d67ae
tejassng <114687385+tejassng@users.noreply.github.com>
2024-12-01 15:34:13 -0500

    Added timer

--- feature_extraction.js
+++ feature_extraction.js
@@ -261,9 +261,11 @@ function calculateDays(creationDate) {
 }
 
 (async () => { //needs to be async otherwise you're just gonna get Promises returned
+    var start = Date.now();
     const url = "https://docs.expo.dev/guides/new-architecture/";
     try {
         const features = await extractURLFeatures(url);
+        console.log("Extraction Time (ms):", Date.now()-start);
         console.log("Extracted Features:", features);
     } catch (error) {
         console.error("Error extracting features:", error);
